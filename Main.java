2791f7de754d3b5bad7772438c942061bc65608e
Hkaua <kaua.hilton@gmail.com>
2023-10-21 00:02:33 -0300

    Terceira atividade

--- Main.java
+++ Main.java
@@ -1,33 +1,30 @@
-public class Main {
-    // Método para calcular a soma dos valores do vetor de forma iterativa
-    public static int somaIterativa(int[] vetor) {
-        int soma = 0;
-        for (int valor : vetor) {
-            soma += valor;
-        }
-        return soma;
-    }
-
-    // Método para calcular a soma dos valores do vetor de forma recursiva
-    public static int somaRecursiva(int[] vetor, int index) {
-        if (index == vetor.length) {
-            return 0;
-        }
-        return vetor[index] + somaRecursiva(vetor, index + 1);
-    }
-
-    public static void main(String[] args) {
-        // Vetor de exemplo com valores definidos por você
-        int[] vetor = {10, 20, 30, 40, 50};
-
-        // Calcula a soma dos valores do vetor de forma iterativa
-        int somaIterativaResult = somaIterativa(vetor);
-
-        // Calcula a soma dos valores do vetor de forma recursiva
-        int somaRecursivaResult = somaRecursiva(vetor, 0);
-
-        // Exibe os resultados das somas no console
-        System.out.println("Soma dos valores do vetor (iterativa): " + somaIterativaResult);
-        System.out.println("Soma dos valores do vetor (recursiva): " + somaRecursivaResult);
-    }
+	import java.util.Locale;
+	import java.util.Scanner; 
+	     
+	public class Main {
+		public	static void main(String[]args) {
+			
+			Locale.setDefault(Locale.US);
+			Scanner SC = new Scanner(System.in); 
+			
+			int idade = SC.nextInt();
+			int soma = 0;
+			int cont = 0;
+			
+			
+			while (idade >= 0) {
+				soma = soma + idade;
+				cont = cont + 1;
+				idade = SC.nextInt();
+				}
+			if (cont > 0) {
+			double media = (double) soma / cont;
+			System.out.printf("%.2f%n", media);
+			}
+			else {
+				System.out.println("impossivel calcular!");
+			}
+				
+			SC.close();
+	}
 }
