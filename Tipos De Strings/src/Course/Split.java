68d2f5fde563b6f3cde481d9485b8f98931a106f
Hkaua <kaua.hilton@gmail.com>
2023-10-29 08:35:48 -0300

    Tipos De Strings

---
+++ Tipos De Strings/src/Course/Split.java
@@ -0,0 +1,14 @@
+package Course;
+
+public class Split {
+	public static void main(String[] args) {
+		String s = "potato apple lemon orange";
+		
+		String[] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		System.out.println(vect[3]);
+	}
+}
