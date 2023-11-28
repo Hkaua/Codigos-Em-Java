5b7b8d1ab6496dbd4fe60db9239f7c57eb3382b6
Kauã Hilton <kaua.hilton@gmail.com>
2023-11-28 03:18:47 -0300

    Atualização do codigo em java - contrutores
    28/11/2023

--- construtores/src/entities/Product.java
+++ construtores/src/entities/Product.java
@@ -4,6 +4,12 @@ public class Product {
 	public String name;
 	public double price;
 	public int quantity;
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
 
 	public double totalValueInStock() {
 		return price * quantity;
