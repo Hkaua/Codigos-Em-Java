4adb275cb54cecc0d9d971354502f2cd77af5906
Kauã Hilton <kaua.hilton@gmail.com>
2024-01-21 01:38:31 -0300

    Encapsulamento
    Encapsulamento em Java refere-se a esconder detalhes internos de uma classe e fornecer uma interface pública controlada. Isso é alcançado usando modificadores de acesso (public, private, protected) para restringir o acesso a membros da classe. O encapsulamento melhora a segurança, manutenção e flexibilidade do código.

---
+++ Encapsulamento/src/entities/Product.java
@@ -0,0 +1,40 @@
+package entities;
+public class Product {
+	public String name;
+	private double price;
+	private int quantity;
+
+	public Product() {
+	}
+
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+public int getQuantity() {
+return quantity;
+}
+}
